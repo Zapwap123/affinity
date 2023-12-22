01fcd2713d6a08cd6c2f07e2fb42e7d3e2bf1744
Z3th <sethanmawen97@gmail.com>
2023-12-22 21:07:52 +0000

    Service update

--- eventscheduler/services/conferenceService.js
+++ eventscheduler/services/conferenceService.js
@@ -1,79 +1,78 @@
 // const { formatTime } = require("../utils/timeFormatter");
 
 // Conference scheduling function
-function scheduleConferenceTalks(talks) {
+function scheduleConferenceTalks(conferenceTalks) {
   const MORNING_SESSION_START = 9 * 60; // 9:00 AM in minutes
   const MORNING_SESSION_END = 12 * 60; // 12:00 PM in minutes
   const AFTERNOON_SESSION_START = 13 * 60; // 1:00 PM in minutes
   const NETWORKING_EVENT_START = 16 * 60; // 4:00 PM in minutes
   const NETWORKING_EVENT_END = 17 * 60; // 5:00 PM in minutes
+  const LUNCH_DURATION = 60; // 60 minutes for lunch
 
   const tracks = [];
-  let trackNumber = 1;
+  let currentTrack = 1;
 
-  function formatTime(minutes) {
-    const hours = Math.floor(minutes / 60);
-    const mins = minutes % 60;
-    return `${String(hours).padStart(2, "0")}:${String(mins).padStart(2, "0")}`;
-  }
-
-  function createTrack() {
-    return {
-      track: trackNumber++,
+  while (conferenceTalks.length > 0) {
+    const track = {
+      trackNumber: currentTrack,
       morning: [],
       afternoon: [],
     };
-  }
 
-  function addToTrack(track, talk, time) {
-    track.push(`${formatTime(time)} ${talk}`);
-  }
-
-  function scheduleTalks(track, talks) {
     let currentTime = MORNING_SESSION_START;
 
-    while (talks.length > 0) {
-      const talk = talks.shift();
+    while (conferenceTalks.length > 0 && currentTime < MORNING_SESSION_END) {
+      const talk = conferenceTalks.shift().replace(/\r$/, ""); // Remove "\r" at the end
       const duration = talk.includes("lightning")
         ? 5
         : parseInt(talk.match(/\d+/)[0], 10);
 
       if (currentTime + duration <= MORNING_SESSION_END) {
-        addToTrack(track.morning, talk, currentTime);
+        track.morning.push(`${formatTime(currentTime)} ${talk}`);
         currentTime += duration;
-      } else if (
-        currentTime >= AFTERNOON_SESSION_START &&
-        currentTime + duration <= NETWORKING_EVENT_START
-      ) {
-        addToTrack(track.afternoon, talk, currentTime);
-        currentTime += duration;
-      } else if (currentTime < AFTERNOON_SESSION_START) {
-        // Move to the afternoon session after lunch
-        addToTrack(track.morning, "12:00PM Lunch", MORNING_SESSION_END);
-        currentTime = AFTERNOON_SESSION_START;
-      } else if (
-        currentTime >= NETWORKING_EVENT_START &&
-        currentTime <= NETWORKING_EVENT_END
-      ) {
-        addToTrack(
-          track.afternoon,
-          "05:00PM Networking Event",
-          NETWORKING_EVENT_START
-        );
-        currentTime = NETWORKING_EVENT_END; // End the day after networking event
       }
     }
-  }
 
-  while (talks.length > 0) {
-    const track = createTrack();
-    scheduleTalks(track, talks);
-    tracks.push(track);
+    // Add lunch to the morning array
+    track.morning.push(`${formatTime(MORNING_SESSION_END)} Lunch`);
+
+    currentTime = AFTERNOON_SESSION_START;
+
+    while (conferenceTalks.length > 0 && currentTime < NETWORKING_EVENT_END) {
+      const talk = conferenceTalks.shift().replace(/\r$/, ""); // Remove "\r" at the end
+      const duration = talk.includes("lightning")
+        ? 5
+        : parseInt(talk.match(/\d+/)[0], 10);
+
+      if (currentTime + duration <= NETWORKING_EVENT_START) {
+        track.afternoon.push(`${formatTime(currentTime)} ${talk}`);
+        currentTime += duration;
+      } else if (currentTime + duration <= NETWORKING_EVENT_END) {
+        track.afternoon.push(`${formatTime(currentTime)} ${talk}`);
+        currentTime += duration;
+        // currentTime = NETWORKING_EVENT_END;
+      } else {
+        track.afternoon.push(
+          `${formatTime(NETWORKING_EVENT_END)} Networking Event`
+        );
+        currentTime = NETWORKING_EVENT_END;
+      }
+    }
+
+    if (track.afternoon.length > 0) {
+      tracks.push(track);
+      currentTrack++;
+    }
   }
 
   return tracks;
 }
 
+function formatTime(minutes) {
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+  return `${String(hours).padStart(2, "0")}:${String(mins).padStart(2, "0")}`;
+}
 // // Test output
 // const schedule = scheduleConferenceTalks(conferenceTalks);
 // schedule.forEach((track, index) => {
